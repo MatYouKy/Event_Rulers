51a310201753639cf4a301fe24b217329ab8765f
MatYou <matyou.kwiatkowsky@gmail.com>
2021-01-04 23:14:35 +0100

    added responses to button etc.

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -1,48 +1,207 @@
 let container;
 let tableElements;
 let menuOpen = false;
+
+let home;
+let aboutus;
+let artist;
+let service;
+let atraction;
+let extras;
+let contact;
+let nav;
+let list;
+let heroText;
+
+
+
 const main = ()=>{
     prepareDomElements();
     prepareDomEvents();
+    helloText();
 }
 function prepareDomElements() {
     container = [...document.querySelectorAll(".container")];
+    list = [...document.querySelectorAll("nav ul li a")];
     tableElements = container[0];
-    
+    heroText = document.querySelector(".hero-text");
+    nav = document.querySelector("nav");
+    contact = document.querySelector("#contact");
+    extras = document.querySelector("#extras");
+    atraction = document.querySelector("#atraction");
+    service = document.querySelector("#service");
+    artist = document.querySelector("#artist");
+    aboutus = document.querySelector("#about");
+    home = document.querySelector("#home");
 }
 function prepareDomEvents() {
-    tableElements.parentElement.addEventListener('click', showMenu)
+    tableElements.parentElement.addEventListener('click', showMenu);
+    window.addEventListener("scroll", onPage);
+    nav.children[0].addEventListener('click', clear)
+    choosed();
+    window.addEventListener('resize', function(){
+            if(window.innerWidth > 1100){
+            clear();
+        }
+    })
     
 }
 
+
+const helloText = () => {
+    setTimeout(() =>{
+        heroText.style.marginLeft = "10%";
+    }, 1000);
+}
 const showMenu = () =>{
     const span = (tableElements.lastChild.previousSibling).firstElementChild
     const line0 = tableElements.firstChild.nextElementSibling;
     const line1 = line0.nextElementSibling;
     const line2 = line1.nextElementSibling;
+    
     if(!menuOpen){
-        menuOpen = true;
         span.style.opacity = 0;
-        line1.style.opacity = 0;
         span.style.transition = ".3s"
-        line0.classList.add("showMenu0");
-        line2.classList.add("showMenu2");
-        
-        
-        
-        
-    }else{
-        menuOpen = false;
-        span.style.opacity = 1;
-        line0.style.transition = ".2s"
-        line2.style.transition = ".2s"
-        line1.style.opacity = 1;
-        line0.classList.remove("showMenu0");
-        line2.classList.remove("showMenu2");
+        addElement();
 
+        
+        
+        // nav.children[2].style.transition = ".3s";
+    }else{
+        span.style.opacity = 1;
+        span.style.transition = ".3s"
+        clear();
 
     }
 }
+const addElement = () => {
+    line1.style.opacity = 0;
+    menuOpen = true;
+    line0.classList.add("showMenu0");
+    line2.classList.add("showMenu2");
+    nav.classList.add("open-menu")
+    nav.children[2].classList.add("mobile-social");
+    nav.children[1].classList.add('nav-block');
+
+    list.forEach(e=>{
+        e.parentElement.style.display = "block";
+        e.parentElement.style.marginTop = "50px";
+        e.parentElement.style.marginLeft = "75px";
+        
+    })
+}
+
+
+const clear =() => {
+    menuOpen = false;
+    line0.style.transition = ".2s"
+    line2.style.transition = ".2s"
+    line1.style.opacity = 1;
+    line0.classList.remove("showMenu0");
+    line2.classList.remove("showMenu2");
+    nav.classList.remove("open-menu")
+    nav.children[2].classList.remove("mobile-social");
+    nav.children[1].classList.remove('nav-block');
+    
+    list.forEach(e=>{
+        e.parentElement.style.display = "";
+        e.parentElement.style.marginTop = "";
+        e.parentElement.style.marginLeft = "";
+    })
+}
+
+
+
+
+const onPage = () => {
+    let windo = window.pageYOffset;
+
+
+    if(windo > 10){
+
+        nav.classList.add('bar');
+        nav.style.transition = ".3s";
+    }else{
+        nav.classList.remove('bar');
+
+    }
+
+    if(aboutus.offsetTop <= windo && artist.offsetTop >windo){
+
+        list[0].classList.add("ac")
+        list[1].classList.remove("ac");
+        list[2].classList.remove("ac");
+        list[3].classList.remove("ac");
+        list[4].classList.remove("ac");
+        list[5].classList.remove("ac");
+        
+    }
+    else if(artist.offsetTop <= windo && service.offsetTop >windo){
+
+        list[0].classList.remove("ac")
+        list[1].classList.add("ac");
+        list[2].classList.remove("ac");
+        list[3].classList.remove("ac");
+        list[4].classList.remove("ac");
+        list[5].classList.remove("ac");
+    }
+    else if(service.offsetTop <= windo && atraction.offsetTop >windo){
+
+        list[0].classList.remove("ac")
+        list[1].classList.remove("ac");
+        list[2].classList.add("ac");
+        list[3].classList.remove("ac");
+        list[4].classList.remove("ac");
+        list[5].classList.remove("ac");
+        
+    }
+    else if(atraction.offsetTop <= windo && extras.offsetTop >windo){
+
+        list[0].classList.remove("ac")
+        list[1].classList.remove("ac");
+        list[2].classList.remove("ac");
+        list[3].classList.add("ac");
+        list[4].classList.remove("ac");
+        list[5].classList.remove("ac");
+    }
+    else if(extras.offsetTop <= windo && contact.offsetTop >windo){
+        list[0].classList.remove("ac")
+        list[1].classList.remove("ac");
+        list[2].classList.remove("ac");
+        list[3].classList.remove("ac");
+        list[4].classList.add("ac");
+        list[5].classList.remove("ac");
+    }
+    else if((contact.offsetTop +0) < windo){
+        list[0].classList.remove("ac")
+        list[1].classList.remove("ac");
+        list[2].classList.remove("ac");
+        list[3].classList.remove("ac");
+        list[4].classList.remove("ac");
+        list[5].classList.add("ac");
+    }
+    else
+    {
+        list[0].classList.remove("ac")
+        list[1].classList.remove("ac");
+        list[2].classList.remove("ac");
+        list[3].classList.remove("ac");
+        list[4].classList.remove("ac");
+        list[5].classList.remove("ac");
+
+    }
+}
+
+const choosed = () =>{
+    for(const el of list){
+        el.addEventListener('click', function (){
+            clear();
+            console.log(this.innerText)
+        })
+    }
+
+}
+
 
 
 document.addEventListener("DOMContentLoaded", main)
